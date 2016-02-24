6e511fdcbf87244268f78d8ffdc9e32769c7df03
Marko Man <darkobas@gmail.com>
2016-02-24 14:06:10 +0100

    Revert "snap: update switcher layout"
    This reverts commit 529cf8701097c5e59a48085f13abdfb3186e25d6.

--- src/com/android/camera/ui/ModuleSwitcher.java
+++ src/com/android/camera/ui/ModuleSwitcher.java
@@ -138,7 +138,7 @@ public class ModuleSwitcher extends RotateImageView
         if (i == GCAM_MODULE_INDEX) {
           setImageResource(R.drawable.ic_switch_camera);
         } else {
-          setImageResource(R.drawable.ic_switcher);
+          setImageResource(mDrawIds[i]);
         }
     }
 
